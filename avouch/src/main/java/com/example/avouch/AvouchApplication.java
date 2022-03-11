c07cd0169ad0d7af01035a0ee3f4ed08dcac3d11
Godson <excanny@yahoo.com>
2022-03-11 20:25:36 +0100

    added springboot template

---
+++ avouch/src/main/java/com/example/avouch/AvouchApplication.java
@@ -0,0 +1,13 @@
+package com.example.avouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvouchApplication.class, args);
+	}
+
+}
